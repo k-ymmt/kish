fe738e21131a3dacaab8a711e60b8c60d27dc959
k-ymmt <account@kymmt.net>
2026-02-06 09:25:52 +0900

    Reorganize lexer tests under tests/lexer

---
+++ tests/lexer.rs
@@ -0,0 +1,10 @@
+#[path = "lexer/boundary_api.rs"]
+mod boundary_api;
+#[path = "lexer/constructor_mode.rs"]
+mod constructor_mode;
+#[path = "lexer/error_types.rs"]
+mod error_types;
+#[path = "lexer/next_token_behavior.rs"]
+mod next_token_behavior;
+#[path = "lexer/source_span_types.rs"]
+mod source_span_types;
